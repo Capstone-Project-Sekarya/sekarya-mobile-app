8bfc72780e65723bd7ee26f7f97d41941ab3452a
Kirigayayat <105103907+Kirigayayat@users.noreply.github.com>
2023-12-12 20:57:20 +0700

    Navigation

--- app/src/main/java/com/android/sekarya_mobile_app/ui/HireFragment.kt
+++ app/src/main/java/com/android/sekarya_mobile_app/ui/HireFragment.kt
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.android.sekarya_mobile_app.R
 
-class HomeFragment: Fragment() {
+class HireFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_hire, container, false)
     }
 }
